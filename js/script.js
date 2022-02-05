45036cd58b3ebe0308360df3134973125c71b86a
Alex Kiborgok <borgokalek14@gmail.com>
2022-02-05 16:34:26 +0300

    ft: query DOM for correct score input

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 const resultDiv = document.getElementById("result-div")
 resultDiv.style.display = "none"
 
+//Get correct answer input
+
+const correctChoice1 = document.getElementById("q1-choice1");
+const correctChoice2 = document.getElementById("q2-choice1");
+const correctChoice3 = document.getElementById("q3-choice1");
+const correctChoice4 = document.getElementById("q4-choice1");
+const correctChoice5 = document.getElementById("q5-choice1");
+const correctChoice6 = document.getElementById("q6-choice1");
+const correctChoice7 = document.getElementById("q7-choice1");
+const correctChoice8 = document.getElementById("q8-choice1");
+const correctChoice9 = document.getElementById("q9-choice1");
+const correctChoice10 = document.getElementById("q10-choice1");
