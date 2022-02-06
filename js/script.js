4a6ea739b4df252a16b736992f2565d85fa3718c
Alex Kiborgok <borgokalek14@gmail.com>
2022-02-06 13:01:17 +0300

    patch: add different questions/answers

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ resultContainer.style.display = "none"
 
 //Get correct answer input
 
-const correctChoice1 = document.getElementById("q1-choice1");
-const correctChoice2 = document.getElementById("q2-choice1");
+const correctChoice1 = document.getElementById("q1-choice3");
+const correctChoice2 = document.getElementById("q2-choice2");
 const correctChoice3 = document.getElementById("q3-choice1");
-const correctChoice4 = document.getElementById("q4-choice1");
-const correctChoice5 = document.getElementById("q5-choice1");
+const correctChoice4 = document.getElementById("q4-choice3");
+const correctChoice5 = document.getElementById("q5-choice2");
 const correctChoice6 = document.getElementById("q6-choice1");
-const correctChoice7 = document.getElementById("q7-choice1");
-const correctChoice8 = document.getElementById("q8-choice1");
-const correctChoice9 = document.getElementById("q9-choice1");
+const correctChoice7 = document.getElementById("q7-choice3");
+const correctChoice8 = document.getElementById("q8-choice2");
+const correctChoice9 = document.getElementById("q9-choice3");
 const correctChoice10 = document.getElementById("q10-choice1");
 
 //Get submit input and add 'click' event listener
